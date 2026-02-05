016309f4866c55eccf54ff1344a7155b255016ea
Danish <itsaws786@gmail.com>
2026-02-05 21:33:49 +0530

    bug fixed2

--- backend/src/index.js
+++ backend/src/index.js
@@ -31,9 +31,9 @@ if(process.env.NODE_ENV==="production"){
     //serves static files 
     app.use(express.static(path.join(__dirname,'../frontend/dist')));
 
-    app.get('/*',(req,res)=>{
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
-    })
+    app.get(/.*/, (req, res) => {
+      res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 server.listen(PORT,()=>{
